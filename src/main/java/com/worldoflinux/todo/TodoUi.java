ced200fa2a2b9f0450bb34cda51b4f2ce09760b1
Catalin Popescu <catalinpopescu@Catalins-iMac.local>
2018-04-30 10:44:45 +0300

    Initial commit

---
+++ src/main/java/com/worldoflinux/todo/TodoUi.java
@@ -0,0 +1,20 @@
+package com.worldoflinux.todo;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.annotations.Theme;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+
+@SpringUI
+@Theme ("valo")
+
+
+
+
+public class TodoUi extends UI {
+
+             @Override
+             protected void init (VaadinRequest VaadinRequest) {
+
+             }
+    }
